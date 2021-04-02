bf91087127723dd7b3743e32f5ed35f263f2cee8
Olexandr Voinalovych <olexandr.voinalovych@gotoinc.co>
2021-04-02 09:36:10 +0300

    Populated the rest of the controllers

--- app/controllers/Api/V1/books_controller.rb
+++ app/controllers/Api/V1/books_controller.rb
@@ -1,7 +1,34 @@
 module Api
   module V1
     class BooksController < ApiController
+      before_action :find_book, only: %i[show destroy]
 
+      def index
+        render json: BookSerializer.new(Book.all).serialized_json
+      end
+
+      def show
+        render json: BookSerializer.new(@book).serialized_json
+      end
+
+      def create
+        @author = Author.create(book_params)
+        render json: BookSerializer.new(@book).serialized_json
+      end
+
+      def destroy
+        render json: BookSerializer.new(@book.destroy).serialized_json
+      end
+
+      private
+
+      def find_book
+        @book = Book.find_by(slug: params[:slug])
+      end
+
+      def book_params
+        params.require(:book).permit(:name, :slug)
+      end
     end
   end
 end
--- app/controllers/Api/V1/genres_controller.rb
+++ app/controllers/Api/V1/genres_controller.rb
@@ -1,7 +1,34 @@
 module Api
   module V1
     class GenresController < ApiController
+      before_action :find_genre, only: %i[show destroy]
 
+      def index
+        render json: GenreSerializer.new(Genre.all).serialized_json
+      end
+
+      def show
+        render json: GenreSerializer.new(@genre).serialized_json
+      end
+
+      def create
+        @author = Author.create(genre_params)
+        render json: GenreSerializer.new(@genre).serialized_json
+      end
+
+      def destroy
+        render json: GenreSerializer.new(@genre.destroy).serialized_json
+      end
+
+      private
+
+      def find_genre
+        @genre = Genre.find_by(slug: params[:slug])
+      end
+
+      def genre_params
+        params.require(:genre).permit(:name, :slug)
+      end
     end
   end
 end
